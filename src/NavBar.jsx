5a07e3d5b8d282c2912f943cb9fa1ecb8d757fca
Micaela Camerini <micaelacamerini19@gmail.com>
2025-06-23 23:23:23 -0300

    Se añadio el logo en el navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,10 +1,11 @@
 import { CartWidget } from './CartWidget';
-
+import logo from './assets/potionLogo.png'; 
 
 export function NavBar() {
   return (
     <header>
     <nav>
+      <img src={logo} alt="Logo de la tienda" style={{ height: '40px' }} />
       <h1>Potions</h1>
       <a href="">Pociones</a>
       <a href="">Recetas</a>
