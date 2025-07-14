8a2d04eadf3c207fe67ad0f05397c020cdfc54ca
Micaela Camerini <micaelacamerini19@gmail.com>
2025-07-14 20:04:48 -0300

    Instalacion de libreria react router

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,15 +1,16 @@
 import { CartWidget } from './CartWidget';
 import logo from './assets/potionLogo.png'; 
+import { Link } from "react-router";
 
 export function NavBar() {
   return (
     <header>
     <nav>
       <img src={logo} alt="Logo de la tienda" style={{ height: '40px' }} />
-      <h1>Potions</h1>
-      <a href="">Pociones</a>
-      <a href="">Recetas</a>
-      <a href="">Nosotros</a>
+      <h1><Link to="./">Potions</Link></h1>
+      <Link to="/pociones">Pociones</Link>;
+      <Link to="/recetas">Recetas</Link>;
+      <Link to="/nosotros">Nosotros</Link>;
       <CartWidget />
     </nav>
     </header>
