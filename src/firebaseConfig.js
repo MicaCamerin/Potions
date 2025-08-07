b15b0b1bc2553decdfe2f2d7e34c53f4bd75f9ae
Micaela Camerini <micaelacamerini19@gmail.com>
2025-08-07 13:16:20 -0300

    configuracion archivo firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVEjBRdsPgNH_SZ78BkrN8LL1YIR8AcxI",
+  authDomain: "potions-a1c1d.firebaseapp.com",
+  projectId: "potions-a1c1d",
+  storageBucket: "potions-a1c1d.firebasestorage.app",
+  messagingSenderId: "827328415323",
+  appId: "1:827328415323:web:e45baf1d1b11bc36e0a17e"
+};
+
+const app = initializeApp(firebaseConfig);
